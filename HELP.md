ca32b82a54347f41895580498719ed2195bf12c1
Austin O'Donoghue <atodonoghue@DESKTOP-M4K0R4I.localdomain>
2021-04-22 16:02:59 -0400

    Lombok integration

---
+++ HELP.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+### Setup MySQL
+docker run -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d -p 3306:3306 mysql 
+
--- src/main/java/com/example/springboottests/model/persistence/WalletEntity.java
+++ src/main/java/com/example/springboottests/model/persistence/WalletEntity.java
@@ -1,12 +1,27 @@
 package com.example.springboottests.model.persistence;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.Locale;
 
+@Data
+@Builder
+@Table("wallet")
 public class WalletEntity {
-    Currency currency = Currency.getInstance(Locale.US);
-
-    public Currency getCurrency() {
-        return Currency.getInstance(Locale.US);
-    }
+    @Id
+    @Column
+    private Long id;
+    @Column
+    @OneToOne(targetEntity = PersonEntity.class)
+    private PersonEntity owner;
+    @Column
+    private Currency currency;
+    @Column
+    private BigDecimal cardinality;
 }
